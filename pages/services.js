aed4aac2068b1733856c4b53588ce35f766a3cb7
tantemac <hege.bjorkedal@gmail.com>
2021-09-21 20:34:22 +0200

    image

--- pages/services.js
+++ pages/services.js
@@ -2,39 +2,35 @@ import Head from "../components/layout/Head";
 import Layout from "../components/layout/Layout";
 import axios from "axios";
 import { BASE_URL } from "../constants/api";
+import Image from "next/image";
 
-export default function Services(props) {
-
-    console.log(props)
-    
+export default function Services({services}) {
     return (
         <Layout>
             <Head title="Tenester"/>
 
             <div className="container">
                 <h1>Tenester</h1>
-                {props.services.map((service) => {
-                return <h3 key={service.id}>{service.title}</h3>;
+                {services.map((service) => {
+
+                return <div key={service.id}>
+                            <h3>{service.title}</h3>
+                            <Image src={service.image} width={300} height={300} alt={service.title} />
+                            <p>Pris: {service.price},-</p>
+                            <p>{service.description}</p>
+                        </div>;
                 })}
             </div>
         </Layout>
  );
-}
+}  
 
 export async function getStaticProps() {
-    let services = [];
-
-    try {
-        const response = await axios.get(BASE_URL);
-        console.log(response.data);
-        services = response.data.data;
-    } catch (error) {
-        console.log(error);
-    }
-
+    const response = await axios.get(BASE_URL);
+    console.log(response.data)
 return {
-    props: {
-        services: services,
-        }, 
-    };
-}
+props: {
+    services: response.data,
+    }, 
+};
+}  
